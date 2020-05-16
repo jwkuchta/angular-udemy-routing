037326b4bd9650bfa53e802b791b48b63ee29bb7
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-16 16:57:18 -0400

    accessed id from params to load single server programmatically

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
+// import { ServersService } from '../servers.service';
+
+// @Component({
+//   selector: 'app-server',
+//   templateUrl: './server.component.html',
+//   styleUrls: ['./server.component.css']
+// })
+// export class ServerComponent implements OnInit {
+//   server: {id: number, name: string, status: string};
+
+//   constructor(private serversService: ServersService) { }
+
+//   ngOnInit() {
+//     this.server = this.serversService.getServer(1);
+//   }
+
+// }
+
+import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -8,12 +28,22 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  
   server: {id: number, name: string, status: string};
+  serverId
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // const id = parseInt(this.route.snapshot.params['id'])
+    const id = +this.route.snapshot.params['id']
+    this.server = this.serversService.getServer(id)
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id'])
+    })
   }
 
 }
