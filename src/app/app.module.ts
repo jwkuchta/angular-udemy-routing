220235ff14b89304f7c5e24183624aa705d78f74
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-18 20:34:09 -0400

    added canDeactivateGuard to protect form data from being descarded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRoutingModule } from './app-routing.module';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 // MOVED TO APP-ROUTING.MODULE.TS
 
@@ -51,7 +52,7 @@ import { AuthService } from './auth.service';
     // RouterModule.forRoot(appRoutes)
     AppRoutingModule
   ],
-  providers: [ServersService, AuthGuard, AuthService],
+  providers: [ServersService, AuthGuard, AuthService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
